ce986da82ff970e41e3890539085aff28757ef13
Tom Petty <hello@anothertompetty.com>
2014-04-21 21:33:01 +0100

    adding hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+//mobile menu toggle
+
+$(document).ready(function() {
+	$('#menu-icon').click(function() {
+		$('.nav').slideToggle(500);
+	});
+});
+
 //expand-bio toggle
 
 $(".expand").click(expandBio)
