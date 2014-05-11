3d379452c9832bc2b137a8d99ab2c26dad44c0ad
Tom Petty <hello@anothertompetty.com>
2014-05-11 11:17:47 +0100

    adding smooth anchor scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,21 @@ $(".expand").click(expandBio)
 	function expandBio() {
 		$(".more-about-me").slideToggle(500);
 	}
+	
+// smooth anchor scrolling
+	
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
