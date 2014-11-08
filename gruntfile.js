ac0350bb14cbaebc7042d017f8428ce3ca9375d1
Tom Petty <hello@anothertompetty.com>
2014-11-08 18:22:13 +0000

    creating dev environment incl initial gruntfile config

---
+++ gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+
+    "use strict";
+
+    grunt.loadNpmTasks("grunt-contrib-sass");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-connect");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+
+    grunt.initConfig({
+
+        sass: {
+
+            dev: {
+                options: {
+                    style: "compressed",
+                    sourcemap : "auto"
+                },
+
+                files : {
+                    "dist/css/app.min.css": "src/scss/app.scss"
+                }
+            }
+        },
+
+        uglify: {
+
+            dev: {
+                options: {
+                    compress: true,
+                    mangle: true,
+                    preserveComments: false
+                },
+
+                files: {
+                    "dist/js/app.min.js" : [	"src/js/app/app.js"]
+                }
+            }
+        },
+
+        connect: {
+
+            server : {
+                options: {
+                    open: true,
+                }
+            }
+        },
+
+        watch: {
+
+            js: {
+                files: ["src/js/**/*.js"],
+                tasks: ["uglify:dev"],
+                options: {
+            			livereload: true,
+            		}
+            },
+
+            scss: {
+                files: ["src/scss/**/*.scss"],
+                tasks: ["sass:dev"],
+                options: {
+            			livereload: true,
+            		}
+            }
+        }
+    });
+
+    grunt.registerTask("make", ["sass:dev", "uglify:dev", "connect:server", "watch"]);
+};
---
+++ src/js/app/app.js
@@ -0,0 +1,1 @@
+alert("id he do?");
