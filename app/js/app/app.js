9efdecddcb51fe5d9ccdbe949d10f75b912d6b9a
Tom Petty <hello@anothertompetty.com>
2015-01-17 22:45:34 +0000

    re-designing folio site (again)

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -1,49 +1,27 @@
 $(document).on('ready', function() {
 
-	console.log('scripts working!');
-
-
 	// hover background bit
 
 	var highlight = $('.highlight');
-	var imageFrame = $('.image-frame');
+  var body = $('body');
 
-	highlight.on('mouseenter', function() {
+  highlight.on('mouseenter', function() {
 
-		console.log('mouse in!');
 		var relatedImage = $(this).data('image');
-		var imagePath = '<img src="img/' + relatedImage + '.jpg">';
-		imageFrame.html(imagePath);
+    body.css({
+      'background' : '#EDEDED url(img/' + relatedImage + '.jpg) no-repeat center center fixed',
+      '-webkit-background-size' : 'cover',
+      '-moz-background-size' : 'cover',
+      '-o-background-size' : 'cover',
+      'background-size' : 'cover'
+    });
 
 	});
 
 	highlight.on('mouseleave', function() {
 
-		console.log('mouse out!');
-		imageFrame.empty();
+		body.css('background', '#EDEDED');
 
 	});
-	
-	// resize fullscreen inline imgs
-
-	var theWindow = $(window);
-	var bg = $("#bg");
-	var imgAspectRatio = bg.width() / bg.height();
-	    			    		
-	function resizeBg() {
-		
-		if ( (theWindow.width() / theWindow.height()) < imgAspectRatio ) {
-		    bg
-		    	.removeClass()
-		    	.addClass('bgheight');
-		} else {
-		    bg
-		    	.removeClass()
-		    	.addClass('bgwidth');
-		}
-					
-	}
-	                   			
-	theWindow.resize(resizeBg).trigger("resize");
 	
 });
