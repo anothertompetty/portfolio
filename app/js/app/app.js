a0f1ce5e012341c128294266126f67160e8f1436
Tom Petty <hello@anothertompetty.com>
2015-09-27 15:23:01 +0100

    restyling site

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -10,34 +10,34 @@ $(document).on('ready', function() {
     
   });
 
-  // link hover turns background to image
-  // hide functionality on mobile devices
+  // // link hover turns background to image
+  // // hide functionality on mobile devices
   
-  if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) == false ) {
+  // if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) == false ) {
 	
-    var highlight = $('.highlight');
-    var body = $('body');
+  //   var highlight = $('.highlight');
+  //   var body = $('body');
 
-    highlight.on('mouseenter', function() {
+  //   highlight.on('mouseenter', function() {
 
-      var relatedImage = $(this).data('image');
+  //     var relatedImage = $(this).data('image');
 
-      body.css({
-        'background' : '#EDEDED url(img/' + relatedImage + '.jpg) no-repeat center center fixed',
-        '-webkit-background-size' : 'cover',
-        '-moz-background-size' : 'cover',
-        '-o-background-size' : 'cover',
-        'background-size' : 'cover'
-      });
+  //     body.css({
+  //       'background' : '#EDEDED url(img/' + relatedImage + '.jpg) no-repeat center center fixed',
+  //       '-webkit-background-size' : 'cover',
+  //       '-moz-background-size' : 'cover',
+  //       '-o-background-size' : 'cover',
+  //       'background-size' : 'cover'
+  //     });
 
-    });
+  //   });
 
-    highlight.on('mouseleave', function() {
+  //   highlight.on('mouseleave', function() {
 
-      body.css('background', '#EDEDED');
+  //     body.css('background', '#EDEDED');
 
-    });
+  //   });
 
-  };
+  // };
     	
 });
