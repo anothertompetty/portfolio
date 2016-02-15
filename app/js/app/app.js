2fdd19574fb23450ba42251e33b987b4011e5f18
Tom Petty <hello@anothertompetty.com>
2016-02-15 17:05:26 +0000

    add imagery to home page

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -19,5 +19,29 @@ $(document).on('ready', function() {
     paginationSpeed : 400,
     singleItem:true,
   });
+
+  // smooth scroll anchors
+
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 500);
+          return false;
+        }
+      }
+    });
+  });
+
+  // sticky nav
+
+  $('.overview').stickem({
+    // start: -20,
+    // offset: -20,
+  });
     	
 });
